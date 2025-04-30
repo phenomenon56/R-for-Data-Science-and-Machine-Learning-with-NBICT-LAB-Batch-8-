8e1c3c48e99bddce5317f6d3cf2282f0505be632
Muhammad Irfanul Islam <irfanmbio15@gmail.com>
2025-04-30 16:51:03 +0600

    Introduction to variable and function

---
+++ RDSML-Day-3 👉 R Programming Basics (Part-1)/r_basics_part1.R
@@ -0,0 +1,12 @@
+# Calculating health parameters 
+my_age = 97
+my_weight = 60
+my_height = 65
+
+x = (my_height * my_age)/ my_weight
+print(x)
+
+
+
+students_height = c(50, 55, 64, 71, 45.25)
+mean(students_height)

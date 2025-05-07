44711207a98f47b51e9a25b49f6f4ba6d9742ad1
Muhammad Irfanul Islam <irfanmbio15@gmail.com>
2025-05-07 16:50:30 +0600

    Day-2 Practice

---
+++ RDSML-Day-4 👉 R Programming Basics (Part-2)/rBasics(Part-2).R
@@ -0,0 +1,51 @@
+# Data Types in R
+# 1. Numeric- Integer / Floating Point
+# 2. String/Character-  "Muhammad Irfanul Islam"
+# 3. Logical
+
+
+a= 11
+b= 7.19
+class(a)
+class(b)
+
+
+
+name = 'NBICT LAB'
+print(name)
+class(name)
+
+
+c= '52'
+print(c+9)
+print(a+9)
+
+# Logical Data Type- TRUE, FALSE (T, F)
+a > b
+class(a > b)
+x = 19
+y = 22-3
+print(x == y)
+print (y >= x)
+
+
+# Logical Operators in R
+# && Logical Operator
+# || Logical Operator
+
+4 == 4 && 5 == 5
+x!= y || x == y
+
+# Simple Mathematical Operators in R
+z = 6+9-8*(2/3)
+print(z)
+
+
+w = (4 + 3 - 3) * 2 /3
+print(w)
+
+p = 4^2
+11/5
+11%%5
+11%/%5
+print(p)

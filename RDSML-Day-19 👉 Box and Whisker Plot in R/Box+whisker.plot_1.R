e422040a1dc45d50d0ee0043b0b862fcf85e291a
Muhammad Irfanul Islam <irfanmbio15@gmail.com>
2025-07-07 16:33:13 +0600

    box+whisker

---
+++ RDSML-Day-19 👉 Box and Whisker Plot in R/Box+whisker.plot_1.R
@@ -0,0 +1,13 @@
+# Anatomy of a boxplot
+# Loading the dataset
+# Converting the variable dose from a numeric to a factor variable
+# Basic box plot
+# Notched box plot
+# Changing outlier, color, shape, and size
+# Choosing which items to display
+# Changing box plot line colors by groups
+# Changing box plot colors by groups
+# Changing the order of items in the legend
+# Box plot with multiple groups
+# Changing box plot colors by groups
+# Changing the box position

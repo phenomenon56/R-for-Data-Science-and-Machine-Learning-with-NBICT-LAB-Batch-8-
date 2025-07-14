8597a9bc2e2c45eb3e6cf31f3df2852d6af86e2b
Muhammad Irfanul Islam <irfanmbio15@gmail.com>
2025-07-14 23:03:18 +0600

    ctl

--- RDSML-Day-22 👉 Central Limit Theorem Using R/central_limit.R
+++ RDSML-Day-22 👉 Central Limit Theorem Using R/central_limit.R
@@ -1,5 +1,29 @@
 # What is a uniform distribution?
 # What is a normal distribution?
-# Generating random numbers of uniform distribution
-# CLT in R
-# For loop in R
+
+# Demonstration of CLT in R
+
+# Generating  10000 numbers of uniform distribution
+r=runif(10000)
+hist(r)
+meanr=mean(r)
+sdr=sd(r)
+
+# Draw three random samples from the distribution
+samples1=sample(r,4)
+samples2=sample(r,4)
+samples3=sample(r,4)
+
+mean(samples1)
+mean(samples2)
+mean(samples3)
+
+# Run a For loop in R
+cl100=mean(sample(r,100))
+cl100
+
+for (i in 1:1000){
+  cl100 <- c(cl100, mean(sample(r,100)))
+}
+
+hist(cl100)

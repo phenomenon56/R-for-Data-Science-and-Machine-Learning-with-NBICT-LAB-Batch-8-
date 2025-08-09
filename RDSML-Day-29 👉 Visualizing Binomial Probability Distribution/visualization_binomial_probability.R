901cf9de81630d17c032b9d3e326615629556b2b
Muhammad Irfanul Islam <irfanmbio15@gmail.com>
2025-08-09 17:55:08 +0600

    visualization_binomial

---
+++ RDSML-Day-29 👉 Visualizing Binomial Probability Distribution/visualization_binomial_probability.R
@@ -0,0 +1,5 @@
+# Creating a sequence of successes
+# Probability of getting a fixed success
+# Calculating the probabilities from no success to all successes
+# Creating a barplot for the successes
+# Visualizing binomial distribution using the visualize package

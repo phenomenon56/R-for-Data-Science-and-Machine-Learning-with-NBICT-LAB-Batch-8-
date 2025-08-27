b524a29bb0adbac008657f69a48d542ae23f9b18
Muhammad Irfanul Islam <irfanmbio15@gmail.com>
2025-08-27 22:29:28 +0600

    two_sample_t_test

--- RDSML-Day-35 👉 Two Sample T Test/two_sample_t_test.R
+++ RDSML-Day-35 👉 Two Sample T Test/two_sample_t_test.R
@@ -1,1 +1,15 @@
-# Two sample T test
+# Two sample T test/Independent sample T test
+# Loading the dataset
+dataset = read.csv("scores.csv")
+
+# Checking the normal distribution
+shapiro.test(dataset$Male)
+shapiro.test(dataset$Female)
+
+# Checking for the homogeneity of variances
+var.test(x = dataset$Male, y = dataset$Female)
+
+# Running the independent sample T test
+t.test(x = dataset$Male, y = dataset$Female, var.equal = T)
+
+# Result should be represented in APA style + box plot

2f0f7879f960db052a5d300ca6eba36c33fb978f
Muhammad Irfanul Islam <irfanmbio15@gmail.com>
2025-10-08 22:46:33 +0600

    Chi_square_part2

---
+++ RDSML-Day-45 👉 Chi-Square Test of Independence in R (Part-2)/Chi_Square_Part2.R
@@ -0,0 +1,58 @@
+# Importing the dataset
+housetasks = read.delim("housetasks.txt", row.names = 1)
+
+# Contingency table visualization using the function ballonplot
+# ballonplot draws a graphical matrix where each cell contains a dot 
+# whose size refelcts therelative magnitude of the corresponding component
+
+# Way 1: Ballon Plot
+# install.packages("gplots")
+library(gplots)
+dt = as.table(as.matrix(housetasks))
+balloonplot(t(dt), main = "Housetasks Visualization",
+            label = F, show.margins = F, xlab = "", ylab = "")
+
+# Contingency table visualization using the function mosaic plot
+# Way 2: Mosaic plot (built-in R package graphics)
+library("graphics")
+mosaicplot(dt, shade = T, las = 2, main = "Housetasks")
+# Blue colour indicates that the observed value is higher than the expected value
+# Red colour indicates that the observed value is lower than the expected value
+# observed value: what we see in dataset
+# expected value: win-win situation: Here four case, so 25% for each case is expected value
+# Chi-square compares between observed and expected value
+
+# Computing the chi-square test in R
+chisq <- chisq.test(housetasks)
+chisq
+
+# Observed counts
+chisq$observed
+
+# Expected counts
+round(chisq$expected,2) # for 2 decimal value
+
+# Residual calculation (Way-1)
+chisq$observed-round(chisq$expected,2)
+
+# Residual calculation (Way-2)
+
+# Standardization of residual (Pearson residual's calculations)
+round(chisq$residuals, 3)
+
+# Adjusted Standardized Residuals calculation (optional)
+chisq$stdres
+
+# Visualize Pearson residuals using the package corrplot
+# install.packages("corrplot")
+library(corrplot)
+corrplot(chisq$residuals, is.cor = FALSE)
+corrplot(chisq$residuals, is.cor = TRUE) # if the pearson residual -1 to +1
+
+# Contribution in percentage (%)
+contrib <- 100 * (chisq$residuals)^2 / chisq$statistic
+round(contrib, 3)
+
+# Visualize the contribution
+corrplot(contrib, is.cor = FALSE)
+

24ee3346745de7b184e51ed672fb5737977c3831
Muhammad Irfanul Islam <irfanmbio15@gmail.com>
2025-10-13 22:54:35 +0600

    Simple_Linear_regression_part2

--- Machine Learning Using R/RDSML-Day-47 👉 Simple Linear Regression in R (Part-2)/Simple_Linear_Regression.R
+++ Machine Learning Using R/RDSML-Day-47 👉 Simple Linear Regression in R (Part-2)/Simple_Linear_Regression.R
@@ -25,7 +25,39 @@ training_set <- subset(dataset, split == TRUE) #two equal means equal, one equal
 test_set <- subset(dataset, split == FALSE)
 
 
-
 # Fitting Simple Linear Regression to the Training set
 regressor <- lm(formula = Salary ~ YearsExperience, 
                 data = training_set)
+
+# Checking the summary of the model
+summary(regressor)
+
+# Predicting the training set results
+y_pred = predict(regressor, newdata = training_set)
+
+# Predicting the test set results
+y_pred = predict(regressor, newdata = test_set)
+
+# Visualizing the Training set results
+library(ggplot2)
+ggplot() + 
+  geom_point(aes(x=training_set$YearsExperience, y=training_set$Salary),
+             colour='red') +
+  geom_line(aes(x=training_set$YearsExperience, y=y_pred),
+            colour='blue') +
+  ggtitle("Salary vs Experience") +
+  xlab('Years of experience') +
+  ylab('Salary')
+
+
+# Visualizing the Test set results
+library(ggplot2)
+ggplot() + 
+  geom_point(aes(x=test_set$YearsExperience, y=test_set$Salary),
+             colour='red') +
+  geom_line(aes(x=test_set$YearsExperience, y=predict(regressor, newdata = test_set)),
+            colour='blue') +
+  ggtitle("Salary vs Experience (Test Set)") +
+  xlab('Years of experience') +
+  ylab('Salary')
+

856525e994ed60cb0234d6f7dffd87a518060755
Muhammad Irfanul Islam <irfanmbio15@gmail.com>
2025-10-15 16:06:10 +0600

    Multiple_Linear_Regression_Part1

---
+++ Machine Learning Using R/RDSML-Day-48 👉 Multiple Linear Regression in R (Part-1)/Multiple_Linear_Regression_Part1.R
@@ -0,0 +1,6 @@
+# Importing the dataset
+# Encoding categorical data
+# Splitting the dataset into the Training set and Test Set
+# Fitting Multiple Linear Regression to the Training Set
+# Checking the summary of the model
+# Predicting the Test set results

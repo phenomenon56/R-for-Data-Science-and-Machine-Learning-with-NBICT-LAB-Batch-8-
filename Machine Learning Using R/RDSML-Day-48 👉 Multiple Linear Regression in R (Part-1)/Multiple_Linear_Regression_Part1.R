fe9721bd45d979e90fb87b6ecaa670efcabb1a9d
Muhammad Irfanul Islam <irfanmbio15@gmail.com>
2025-10-15 22:43:36 +0600

    Multiple_Linear_Regression_Part1

--- Machine Learning Using R/RDSML-Day-48 👉 Multiple Linear Regression in R (Part-1)/Multiple_Linear_Regression_Part1.R
+++ Machine Learning Using R/RDSML-Day-48 👉 Multiple Linear Regression in R (Part-1)/Multiple_Linear_Regression_Part1.R
@@ -1,6 +1,21 @@
 # Importing the dataset
-# Encoding categorical data
+dataset <- read.csv("50_Startups.csv")
+
+# Encoding categorical data (here State is the Categorical Data)
+dataset$State <- factor(dataset$State,
+                        levels = c("New York", "California", "Florida"),
+                        labels = c(1,2,3))
+
 # Splitting the dataset into the Training set and Test Set
-# Fitting Multiple Linear Regression to the Training Set
+library(caTools)
+set.seed(123)
+split <- sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set <- subset(dataset, split == T)
+test_set <- subset(dataset, split == F)
+
+# Fitting Multiple Linear Regression to the Training Set/Model preparation
+regressor <- lm(formula = Profit~ R.D.Spend + Administration + Marketing.Spend + State, data = training_set)
+
 # Checking the summary of the model
-# Predicting the Test set results
+summary(regressor)
+
